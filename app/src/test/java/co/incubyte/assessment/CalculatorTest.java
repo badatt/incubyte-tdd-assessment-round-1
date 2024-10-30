842054b8644e145b8b39268f9d46388102b538e4
badatt <dev.badatt@gmail.com>
2024-10-30 09:02:04 +0530

    a simple empty test

---
+++ app/src/test/java/co/incubyte/assessment/CalculatorTest.java
@@ -0,0 +1,11 @@
+package co.incubyte.assessment;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void simpleTest() {
+        System.out.println("Simple Test");
+    }
+}
