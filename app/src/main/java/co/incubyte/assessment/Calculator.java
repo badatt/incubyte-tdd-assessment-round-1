db2994088b610b0246d4190c411d44ab5eb5b4b2
badatt <dev.badatt@gmail.com>
2024-10-30 09:33:29 +0530

    test empty string

--- app/src/main/java/co/incubyte/assessment/Calculator.java
+++ app/src/main/java/co/incubyte/assessment/Calculator.java
@@ -3,6 +3,9 @@ package co.incubyte.assessment;
 public class Calculator {
 
     public int add(String numbers) {
+        if(numbers.isEmpty() || numbers.isBlank()) {
+            return 0;
+        }
         return 0;
     }
 }
