05c5ad19a65521867d69f7ce701e177cc2251efd
badatt <dev.badatt@gmail.com>
2024-10-30 20:37:39 +0530

    handle negative numbers in the string

--- app/src/main/java/co/incubyte/assessment/Calculator.java
+++ app/src/main/java/co/incubyte/assessment/Calculator.java
@@ -1,17 +1,51 @@
 package co.incubyte.assessment;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Calculator {
 
-    public int add(String numbers) {
+    private final String pattern = "^\\\\\\\\(\\S)\n";
+
+    public int add(String numbers) throws Exception {
         if(numbers.isEmpty() || numbers.isBlank()) {
             return 0;
         }
-        Optional<Integer> sum = Arrays.stream(numbers.split("[\\n,]"))
-                .map(s -> Integer.parseInt(s.trim()))
-                .reduce(Integer::sum);
+        return calculateSum(sanitizeInput(numbers), findDelimiterIfExists(numbers));
+    }
+
+    private String findDelimiterIfExists(String numbers) {
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(numbers);
+        if(m.find())
+            return m.group(1);
+        return ",";
+    }
+
+    private String sanitizeInput(String numbers) {
+        return numbers.replaceAll(pattern, "");
+    }
+
+    private int calculateSum(String numbers, String delimiter) throws Exception {
+        List<Integer> positiveNumbers = new ArrayList<>();
+        List<Integer> negativeNumbers = new ArrayList<>();
+        Arrays.stream(numbers.split("[\\n" + delimiter + "]"))
+                .map(String::trim)
+                .forEach(s -> {
+                    Integer n = Integer.parseInt(s);
+                    if(n > 0)
+                        positiveNumbers.add(n);
+                    else
+                        negativeNumbers.add(n);
+                });
+        if(!negativeNumbers.isEmpty())
+            throw new Exception("negative numbers not allowed " + negativeNumbers.stream().map(Object::toString).collect(Collectors.joining(",")));
+        Optional<Integer> sum = positiveNumbers.stream().reduce(Integer::sum);
         return sum.orElse(0);
     }
 }
--- app/src/test/java/co/incubyte/assessment/CalculatorTest.java
+++ app/src/test/java/co/incubyte/assessment/CalculatorTest.java
@@ -1,23 +1,44 @@
 package co.incubyte.assessment;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
 
 public class CalculatorTest {
 
     Calculator calculator = new Calculator();
 
     @Test
-    public void handleEmptyString() {
+    public void handleEmptyString() throws Exception {
         assert calculator.add("") == 0;
     }
 
     @Test
-    public void handleCommaSeparatedNumbers() {
+    public void handleCommaSeparatedNumbers() throws Exception {
         assert calculator.add("2, 3, 5,     10") == 20;
     }
 
     @Test
-    public void handleCommaAndNewlineSeparatedNumbers() {
+    public void handleCommaAndNewlineSeparatedNumbers() throws Exception {
         assert calculator.add("2, 3, 5,     10\n6    \n 7  \n 3, 4\n2,  8\n    9,1") == 60;
+    }
+
+    @Test
+    public void handleCustomDelimiterAndNewlineSeparatedNumbers() throws Exception {
+        String input = "\\\\;\n2; 3; 5;     10\n6    \n 7  \n 3; 4\n2;  8\n    9;1";
+        assert calculator.add(input) == 60;
+    }
+
+    @Test
+    public void handleCustomDelimiterAndNewlineSeparatedNumbersWithNegativeNumbers() {
+        Exception ex = Assertions.assertThrows(Exception.class, () -> calculator.add("\\\\;\n2; 3; 5;     10\n6    \n -7  \n -3; 4\n-2;  -8\n    -9;1"));
+        assert Objects.equals(ex.getMessage(), "negative numbers not allowed -7,-3,-2,-8,-9");
+    }
+
+    @Test
+    public void handleCommaAndNewlineSeparatedNumbersWithNegativeNumbers() {
+        Exception ex = Assertions.assertThrows(Exception.class, () -> calculator.add("2, 3, 5,     -10\n6    \n 7  \n -3, 4\n2,  8\n    9,1"));
+        assert Objects.equals(ex.getMessage(), "negative numbers not allowed -10,-3");
     }
 }
